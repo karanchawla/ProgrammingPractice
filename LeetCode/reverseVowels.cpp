397684066f142cb7eb5e4043c84994608885dddf
Karan Chawla <karangc2@illinois.edu>
2017-03-09 09:33:14 -0600

    minor fix

--- LeetCode/reverseVowels.cpp
+++ LeetCode/reverseVowels.cpp
@@ -15,14 +15,14 @@ std::string Solution::reverseVowels(std::string s)
 	std::stack<char> vowels;
 	for (std::string::iterator it = s.begin(); it < s.end(); ++it)
 	{
-		if(*it=='a' || *it=='e' || *it=='i' || *it=='o' || *it=='u')
+		if(*it=='a' || *it=='e' || *it=='i' || *it=='o' || *it=='u' || *it=='A' || *it=='E' || *it=='I' || *it=='O' || *it=='U')
 			vowels.push(*it);
 	}
 
 	std::string newString;
 	for (std::string::iterator it = s.begin(); it < s.end(); ++it)
 	{
-		if(*it=='a' || *it=='e' || *it=='i' || *it=='o' || *it=='u')
+		if(*it=='a' || *it=='e' || *it=='i' || *it=='o' || *it=='u' || *it=='A' || *it=='E' || *it=='I' || *it=='O' || *it=='U')
 		{			
 			newString.push_back(vowels.top());
 			vowels.pop();
@@ -39,7 +39,7 @@ std::string Solution::reverseVowels(std::string s)
 int main(void)
 {
 	Solution sol;
-	std::string s = "Fear is a mind Killer.";
+	std::string s = "aA";
 	std::string t;
 	t = sol.reverseVowels(s);
 	std::cout << t << std::endl;
