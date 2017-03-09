4edc419334bc3adff3f939ec65b2a87e09c63fc1
Karan Chawla <karangc2@illinois.edu>
2017-03-09 16:00:57 -0600

    find duplicate

---
+++ LeetCode/findDuplicate.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <algorithm> 
+
+using namespace std;
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int ans;
+        for(std::vector<int>::iterator i=nums.begin(); i!=nums.end()-1;++i)
+        {          
+            if(*i==*(i+1))
+            {
+                ans = *i;
+                break;
+            }
+            
+        }
+        return ans;
+    }
+};
+
+int main()
+{
+    Solution sol;
+    std::vector<int> v;
+    v.push_back(1);
+    v.push_back(2);
+    v.push_back(2);
+    
+    cout << sol.findDuplicate(v);
+
+    return 0;
+}
